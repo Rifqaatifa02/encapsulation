1a591a79dad705e7a17ee1005190b58fe42d37f8
Rifqaatifa02 <127828463+Rifqaatifa02@users.noreply.github.com>
2023-04-22 15:40:24 +0700

    Add files via upload

---
+++ Mahasiswa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Encapsulation
+{
+    internal class Mahasiswa
+    {
+        public static int jumlahMahasiswa = 0;
+        public string nama;
+        public int umur;
+
+        public Mahasiswa(string Nama, int Umur)
+        {
+            this.nama = Nama;
+            this.umur = Umur;
+            jumlahMahasiswa++;
+        }
+        public string NamaMahasiswa
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public int UmurMahasiswa
+        {
+            get { return umur; }
+            set { umur = value; }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa mhs1 = new Mahasiswa("Budi", 21);
+            Mahasiswa mhs2 = new Mahasiswa("Asep", 20);
+            Mahasiswa mhs3 = new Mahasiswa("Yanto", 23);
+
+            Console.WriteLine("Total Mahasiwa : {0}", Mahasiswa.jumlahMahasiswa);
+            Console.WriteLine("Nama mahasiswa 1 : {0} berusia {1} tahun.", mhs1.NamaMahasiswa, mhs1.UmurMahasiswa);
+            Console.WriteLine("Nama mahasiswa 2 : {0} berusia {1} tahun.", mhs2.NamaMahasiswa, mhs2.UmurMahasiswa);
+            Console.WriteLine("Nama mahasiswa 3 : {0} berusia {1} tahun.", mhs3.NamaMahasiswa, mhs3.UmurMahasiswa);
+            Console.ReadKey();
+        }
+    }
+}
